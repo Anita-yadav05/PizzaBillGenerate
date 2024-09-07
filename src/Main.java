8506da68d4b36cc81f31007e3ee737174d75faef
Anita <anitakumariyadav220@gmail.com>
2024-09-07 14:14:41 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+ /*    pizza basepizza=new pizza(true);
+     basepizza.addExtraCheese();
+     basepizza.addExtraToppings();
+     basepizza.takeAway();
+     basepizza.getBill();
+    }
+}*/
+        deluxpizza dp = new deluxpizza(true);
+        dp.addExtraCheese();
+        dp.addExtraToppings();
+        dp.getBill();
+    }
+}
